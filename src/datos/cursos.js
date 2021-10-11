fb81afb3a0da1f1a62020eb0094b80b40a5f0890
rherrera94 <rafael.herrera@outlook.com.ar>
2021-10-11 12:55:52 -0300

    se soluciona bug

---
+++ src/datos/cursos.js
@@ -0,0 +1,7 @@
+const cursos=[
+    {"curso":"Curso de programación javascript", "fechainicio":"10/22/2021"},
+    {"curso":"Curso de programación en ReactJs", "fechainicio":"10/13/2021"},
+    {"curso":"Curso data analytics", "fechainicio":"10/11/2021"}
+]
+
+module.exports=cursos;
